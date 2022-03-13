69f9a5fc4c0ded6efe62f7fa78edee51ea409dd5
princesscherukei <princesscherukei@gmail.com>
2022-03-13 14:39:01 -0700

    my commit on task 5, 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-	*main - entry point
-*
-*Return: 0 after printing the function
-*/
+ *main - Entry point
+ *My hello, printf task
+ *Return: return 1 after program execution
+ */
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
