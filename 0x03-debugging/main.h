df504e1a4211894006434b2cceb996cb9d57b1ba
princesscherukei <princesscherukei@gmail.com>
2022-03-17 02:38:57 -0700

    my commit for task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,9 +3,8 @@
 
 #include <stdio.h>
 
-void positive_or_negative(int i)
-int largrst_number(int a, int b, int c);
-void print_remaining_days)int monthh, int day, int year);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-
+void print_remaining_days(int month, int day, int year);
 #endif
