4d93d336cdd39f8f2585d032f33671902de3ef46
princesscherukei <princesscherukei@gmail.com>
2022-03-26 00:57:56 -0700

    my commit on task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,25 +5,26 @@
  * Return: the pointer dest.
  */
 
-Char *leet (char *s)
+char *leet(char *s)
 {
 int count = 0, i;
 int lower_case[] = {97, 101, 111, 116, 108};
-int upper_case[]={65, 69, 79, 84, 76};
-int numbers [] = {52, 51, 48, 55, 49};
+int upper_case[] = {65, 69, 79, 84, 76};
+int numbers[] = {52, 51, 48, 55, 49};
 
 
 while (*(s + count) != '\0')
 {
 for (i = 0; i < 5; i++)
 {
-if (*(s + count) == lower_case [i] || *(s + count) == upper_case[i])
+if (*(s + count) == lower_case[i] || *(s + count) == upper_case[i])
 {
-*(s + count) = numbers [i];
+*(s + count) = numbers[i];
 break;
 }
 }
 count++;
-} 
-retun (s);
+}
+
+return (s);
 }
